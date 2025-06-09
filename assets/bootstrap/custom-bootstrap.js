3cb737fcb417d883f98acc9e0d9573e66d5f2634
Enaellia <agnes.hourcade@gmail.com>
2025-06-09 10:23:57 +0200

    boostrap custom

---
+++ assets/bootstrap/custom-bootstrap.js
@@ -0,0 +1,8 @@
+// assets/bootstrap/custom-bootstrap.js
+import Modal from 'bootstrap/js/dist/modal';
+import Carousel from 'bootstrap/js/dist/carousel';
+
+window.bootstrap = {
+  Modal,
+  Carousel
+};
